b89752831ed6652bf91f29889729b5583a5fac61
Elwin Stephan <elwin.stephan@gmail.com>
2019-02-24 13:01:50 +0100

    message if no menus

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -38,6 +38,12 @@ class TelegramController extends Controller
             $text = 'Menu vom ' . Carbon::today()->formatLocalized('%d.%m') . ":\n\n";
             $menus = $cafeteria->menus()->where('date', Carbon::today())->get();
 
+            if ($menus->count() > 0) {
+                $text = $text . $menus->implode('description', "\n\n");
+            } else {
+                $text = 'Heute gibt hier leider kein Menu, schau bitte an einem anderen Tag wieder vorbei oder versuche eine andere Mensa.';
+            }
+
             Telegram::sendMessage([
                 'chat_id' => $request->message['chat']['id'],
                 'text' => $text . $menus->implode('description', "\n\n")
